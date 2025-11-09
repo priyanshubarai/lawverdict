654a065c39f3e5ebefc0868bbd6dbae2c6c5ed9c
priyanshubarai <priyanshubarai45@gmail.com>
2025-11-09 19:51:49 +0530

    home page modified

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -54,7 +54,7 @@ const NavBar = () => {
             <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
               <li>
                 <a
-                  href="#"
+                  href="/"
                   className="block rounded-sm bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
                   aria-current="page"
                 >
@@ -67,6 +67,14 @@ const NavBar = () => {
                   className="block rounded-sm px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
                 >
                   Dashboard
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/about"
+                  className="block rounded-sm px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                >
+                  About
                 </a>
               </li>
             </ul>
